48c91d0c57cd33e0af8ded519b2334093174522f
hector <hmgarcia06@gmail.com>
2023-02-26 00:26:44 -0500

    Contract - Added constants for errors and write file utility

---
+++ packages/contracts-v0/utils/constants/error.ts
@@ -0,0 +1,4 @@
+// Here we will define string variables for error messages
+export const UNLOCK_TIME_SHOULD_BE_FUTURE = "Unlock time should be in the future"
+export const YOU_CANT_WITHDRAW_YET = "You can't withdraw yet"
+export const YOU_ARENT_THE_OWNER = "You aren't the owner"
