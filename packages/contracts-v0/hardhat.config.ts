471c1c2cbbab1a486191f9f9aca371289b6c53a1
hector <hmgarcia06@gmail.com>
2023-02-26 00:26:19 -0500

    Made changes to hardhat.config to have another account aside from the deployer one

--- packages/contracts-v0/hardhat.config.ts
+++ packages/contracts-v0/hardhat.config.ts
@@ -1,8 +1,46 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-ethers';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+    INFURA_ID,
+    GOERLI_PRIVATE_KEY,
+    GOERLI_PRIVATE_KEY2,
+    GOERLI_INFURA_URL,
+    MAINNET_PRIVATE_KEY,
+    MAINNET_INFURA_URL,
+    ETHERSCAN_API_KEY,
+} = process.env;
+
+const url = `${GOERLI_INFURA_URL}${INFURA_ID}`;
+
+const accounts = [`0x${GOERLI_PRIVATE_KEY}`];
+// NOTE: this private key is for the second account(accounts2)
+// that is not the deployer to test the contract
+// const accounts = [`0x${GOERLI_PRIVATE_KEY2}`];
+
+const goerli = {
+    url,
+    accounts,
+};
+
+const networks = {
+    goerli,
+};
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: {
+        compilers: [{ version: '0.8.17', settings: {} }],
+    },
+    networks: {
+        goerli,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
