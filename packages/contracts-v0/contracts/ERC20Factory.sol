3c8479b635ca35fe8d53d1764f2f3340f4c3e037
hector <hmgarcia06@gmail.com>
2023-02-26 22:19:33 -0500

    Contract - Created ERC20Factory contract

---
+++ packages/contracts-v0/contracts/ERC20Factory.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Factory is ERC20 {
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+  
+    function mint(address account, uint256 amount) public {
+      console.log("ERC20Factory mint for account % and amount of s%", account, amount);
+      _mint(account, amount);
+    }
+
+
+}
