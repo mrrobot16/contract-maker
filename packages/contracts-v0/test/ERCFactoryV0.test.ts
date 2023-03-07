356d9e37984f0dcb57e5c72ccd68f09c99b4e819
hector <hmgarcia06@gmail.com>
2023-03-06 23:17:11 -0500

    Contract - Added test for create ERC20 contract by calling createERC20 in ERCFactoryV0

--- packages/contracts-v0/test/ERCFactoryV0.test.ts
+++ packages/contracts-v0/test/ERCFactoryV0.test.ts
@@ -8,18 +8,18 @@ import erc20FactoryV0 from '../artifacts/contracts/factory/ERC20FactoryV0.sol/ER
 describe('ERCFactoryV0 Contract', function () {
     let signers: SignerWithAddress[] | Signer[];
     let owner: SignerWithAddress | Signer;
-    
-    let storage: ContractFactory;
-    let ercStorage: Contract;
 
     let contract: ContractFactory;
     let ercFactory: Contract;
 
+    let erc20Factory: ContractFactory;
+    // let erc20Factory: Contract;
+
     let address: string;
-    let ercStorageAddress: string;
 
     let erc20Tokens: any[];
     let erc20Token: any;
+    let erc20TokenTotalSupply: BigNumber;
 
     let ercFactoryProxyAdmin: ContractFactory;
     let proxyAdmin: Contract;
@@ -34,20 +34,15 @@ describe('ERCFactoryV0 Contract', function () {
         signers = await ethers.getSigners();
         owner = signers[0];
 
-        storage = await ethers.getContractFactory(
-          "ERCFactoryStorage"
-        );
-        ercStorage = await storage.deploy();
-        await ercStorage.deployed();
-        ercStorageAddress = ercStorage.address;
-
         contract = await ethers.getContractFactory(
             "ERCFactoryV0"
         );
-        ercFactory = await contract.deploy({value: ethers.utils.parseEther("0.00012345")});    
-        await ercFactory.deployed();
+        erc20Factory = await ethers.getContractFactory("ERC20FactoryV0")
+        ercFactory = await contract.deploy();    
+        // await ercFactory.deployed();
         // await ercFactory.initialize(ercStorageAddress);
         await ercFactory.initialize();
+        
         address = ercFactory.address
 
     });
@@ -79,7 +74,16 @@ describe('ERCFactoryV0 Contract', function () {
         });
 
         it("Should create ERC20 token", async function () {
-            await ercFactory.createERC20("TestFactoryTokenV0", "TFT", ethers.utils.parseEther(".0000067890"));
+            const bytecode = erc20Factory.bytecode;
+            const salt = ethers.utils.randomBytes(32);
+            const createERC20 = await ercFactory.createERC20(bytecode, salt);
+            const tx = await createERC20.wait();
+            const data = tx.logs[0].data;
+            const contractAddress = "0x" + data.slice(26, data.length);
+            const deployedERC20Factory = await ethers.getContractAt("ERC20FactoryV0", contractAddress);
+            await deployedERC20Factory.initialize()
+            const isInitialized = await deployedERC20Factory.isInitialized();
+            expect(isInitialized).to.equal(true);
         });
 
         it.skip("Should have correct count of erc20 tokens after ercFactory.createERC20()", async function () {
@@ -96,7 +100,7 @@ describe('ERCFactoryV0 Contract', function () {
         });
     });
 
-    describe('Deployment ERCFactoryProxyAdmin', function () {
+    describe.skip('Deployment ERCFactoryProxyAdmin', function () {
         
         it("Should deploy ERCFactoryProxyAdmin correctly", async function () {
             ercFactoryProxyAdmin = await ethers.getContractFactory(
@@ -110,7 +114,7 @@ describe('ERCFactoryV0 Contract', function () {
         }); 
     });
 
-    describe('Deployment ERCFactoryProxy', function () {
+    describe.skip('Deployment ERCFactoryProxy', function () {
         
         it("Should deploy ERCFactoryProxy correctly", async function () {
             ercFactoryProxy = await ethers.getContractFactory(
