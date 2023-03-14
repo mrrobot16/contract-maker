27d05d2aa05802b604b14e97391c8b70a7ee1875
hector <hmgarcia06@gmail.com>
2023-03-14 11:07:12 -0400

    Contract - In scripts deploy-factory & createERC20 changed gasLimit to be estimated rather than hardcoded

--- packages/contracts-v0/scripts/createERC20.ts
+++ packages/contracts-v0/scripts/createERC20.ts
@@ -1,12 +1,11 @@
 import { ethers } from 'hardhat';
+import { BigNumber } from 'ethers';
 
 import {
     Network,
 } from '../utils/constants';
-
 import { writeJSONFile } from '../utils/files';
 import ercFactoryV0 from './deployments/goerli/ercFactoryV0.json';
-import { BigNumber } from 'ethers';
 
 const MockERC20Token = {
     name: "TestFactoryTokenV0",
@@ -14,14 +13,12 @@ const MockERC20Token = {
     totalSupply: BigNumber.from("1000000000000000000000000"),
 }
 
-const createERC20Value = ethers.utils.parseEther("0.000001");
-const gasLimit = 1000000;
-
 async function main() {
     const ERCFactoryV0 = await ethers.getContractAt("ERCFactoryV0", ercFactoryV0.address);
     console.log("ERCFactoryV0 contract address: ", ercFactoryV0.address);
-    const erc20Factory = await ethers.getContractFactory("ERC20FactoryV0")
-    const bytecode = erc20Factory.bytecode;
+    const ERC20Factory = await ethers.getContractFactory("ERC20FactoryV0");
+    const erc20Factory = await ERC20Factory.deploy();
+    await erc20Factory.deployed();
     const salt = ethers.utils.randomBytes(32);
     const { name, symbol, totalSupply } = MockERC20Token;
     const paramsTypes = [
@@ -35,20 +32,23 @@ async function main() {
         // totalSupply
     ];
     const initializer = ethers.utils.defaultAbiCoder.encode(paramsTypes, paramsInitiliazer);
-    const createERC20 = await ERCFactoryV0.createERC20(bytecode, salt, initializer, { gasLimit: 1000000 });
+    const gasCost = await ERCFactoryV0.estimateGas.createERC20(erc20Factory.address, salt, initializer);
+    const createERC20 = await ERCFactoryV0.createERC20(erc20Factory.address, salt, initializer, { gasLimit: gasCost.toNumber() });
     const tx = await createERC20.wait();
-    console.log('tx: ', Object.keys(tx));
     const logs = tx.logs;
-    const data = logs[0].data;
-    const contractAddress = "0x" + data.slice(26, data.length);
+    const data = logs[logs.length -1].data;
+    // NOTE: Need to fix this slice so that the final out has the correct length of characters for the address
+    const contractAddress = "0x" + (data.slice(26, data.length)).slice(0,26);
+    console.log("contractAddress: ", contractAddress);
+    // NOTE: Below line is not working because we are not passing correct address to getContractAt.
     const deployedERC20Factory = await ethers.getContractAt("ERC20FactoryV0", contractAddress);
     const isInitialized = await deployedERC20Factory.isInitialized();
-    console.log("isInitialized: ", isInitialized);
     const filePath = `scripts/deployments/${Network}/createERC20.json`;
     const fileData = {
+        contract: "ERC20FactoryV0",
         network: Network,
-        address: ercFactoryV0.address,
-        etherscan_url: `https://${Network}.etherscan.io/address/${ercFactoryV0.address}`,
+        address: contractAddress,
+        etherscan_url: `https://${Network}.etherscan.io/address/${contractAddress}`,   
     };
     writeJSONFile(filePath, fileData)
 }
--- packages/contracts-v0/scripts/deploy-factory.ts
+++ packages/contracts-v0/scripts/deploy-factory.ts
@@ -8,13 +8,11 @@ import { writeJSONFile } from '../utils/files';
 
 async function main() {
     const ERCFactoryV0 = await ethers.getContractFactory("ERCFactoryV0");
-    const ercFactoryV0 = await ERCFactoryV0.deploy({ gasLimit: 1000000});
-    // console.log("ERCFactoryV0 deployed to:", ercFactoryV0);
+    const ercFactoryV0 = await ERCFactoryV0.deploy();
     await ercFactoryV0.deployed();
     console.log(`ERCFactoryV0 deployed to ${ercFactoryV0.address}`);
     const filePath = `scripts/deployments/${Network}/ercFactoryV0.json`;
     const fileData = {
-        // timestamp: currentTimestampInSeconds,
         network: Network,
         address: ercFactoryV0.address,
         etherscan_url: `https://${Network}.etherscan.io/address/${ercFactoryV0.address}`,
